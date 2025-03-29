eaa94f120cff32b22bb246d34ff25f18f50ed09e
Kamalakar Chavan <chavankamalakar@gmail.com>
2025-03-29 18:58:29 +0530

    intial commit

---
+++ src/main/java/com/example/springbootappgit/SpringBootAppGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootappgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppGitApplication.class, args);
+	}
+
+}
