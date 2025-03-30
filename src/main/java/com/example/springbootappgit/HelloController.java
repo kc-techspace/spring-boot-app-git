a7bcae5aa88dced0fbf8881beb174edcdb1d819b
Kamalakar Chavan <chavankamalakar@gmail.com>
2025-03-30 15:21:58 +0530

    added pin

--- src/main/java/com/example/springbootappgit/HelloController.java
+++ src/main/java/com/example/springbootappgit/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
 
     @GetMapping("/first-method")
     public String firstMethod(){
-        return "Hello, First Method from Maharashtra, India";
+        return "Hello, First Method from Maharashtra, India - 412334";
     }
 }
